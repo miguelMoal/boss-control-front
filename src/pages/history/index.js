0bc6bca45cb4d32ffe12b4698517e318ff58bddb
miguel522react <53583059+miguel522react@users.noreply.github.com>
2023-05-12 07:37:08 -0600

    update history

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -9,7 +9,7 @@ import {
   Text,
   HandleStatus,
   CustomButton,
-  Spinner,
+  ItemHistory,
 } from "@/components";
 //HOC
 import { ProtectedRoute } from "@/HOC";
@@ -100,59 +100,7 @@ const History = () => {
           className="scroll"
         >
           {histories?.msg.map((history, index) => (
-            <>
-              <Flex
-                key={history._id}
-                pd="20px"
-                style={{
-                  position: "relative",
-                  border: `1px solid ${primaryColor}`,
-                  borderRadius: "5px",
-                }}
-                direction="column"
-                gap="20px"
-                mt="30px"
-              >
-                <Flex
-                  w="fit-content"
-                  style={{ position: "absolute", top: "-20px" }}
-                  bg={primaryColor}
-                  pd="0px 30px"
-                  h="40px"
-                  align="center"
-                >
-                  <Text color="white" weight="bold">
-                    {transformDate(history.date)}
-                  </Text>
-                </Flex>
-                <Flex direction="column">
-                  {history.products?.map((prod) => (
-                    <Flex justify="space-between" mt="15px">
-                      <Text weight="bold" w="33.3%">
-                        {prod?.productId?.name}
-                      </Text>
-                      <Text
-                        weight="bold"
-                        w="33.3%"
-                        style={{ textAlign: "center" }}
-                      >
-                        Cantidad: {prod.quantity}
-                      </Text>
-                      <Text
-                        weight="bold"
-                        w="33.3%"
-                        style={{ textAlign: "end" }}
-                      >
-                        $ {prod.amount}
-                      </Text>
-                    </Flex>
-                  ))}
-                </Flex>
-              </Flex>
-              {index === histories.msg.length - 1 && isFetchingMore && (
-                <Spinner size="lg" color={success} />
-              )}
-            </>
+            <ItemHistory history={history} key={history.date + index} />
           ))}
         </Flex>
         <Flex justify="flex-end" mt="10px">
