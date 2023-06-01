1f83267159584e849ee42678fbf1f6fcb6c38a7b
miguel522react <53583059+miguel522react@users.noreply.github.com>
2023-06-01 14:27:50 -0600

    update legend

--- src/components/PieChart/index.js
+++ src/components/PieChart/index.js
@@ -5,12 +5,11 @@ const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const PieChart = ({ data, series, width = 500, height = 300 }) => {
   const options = {
+    colors: ["#fd5d93", "#1d8cf8", "#ff8d72", "#00bf9a", "#ff8d72"],
     legend: {
       position: "right",
       labels: {
-        style: {
-          colors: ["#FF0000", "#00FF00", "#0000FF"], // Cambiar el color de los elementos de leyenda
-        },
+        colors: "white", // Cambiar el color de los elementos de leyenda
       },
     },
     chart: {
@@ -18,14 +17,14 @@ const PieChart = ({ data, series, width = 500, height = 300 }) => {
     },
     series: series,
     labels: data,
-    dataLabels: {
-      // enabled: true,
-      // formatter: function (val, opts) {
-      //   console.log(">>>", opts);
-      //   const seriesName = opts.w.globals.seriesTotals[opts.seriesIndex];
-      //   return seriesName;
-      // },
-    },
+    // dataLabels: {
+    // enabled: true,
+    // formatter: function (val, opts) {
+    //   console.log(">>>", opts);
+    //   const seriesName = opts.w.globals.seriesTotals[opts.seriesIndex];
+    //   return seriesName;
+    // },
+    // },
   };
 
   return (
