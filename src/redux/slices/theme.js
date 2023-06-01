a1bd068cc3e69ab1251638f412bd4c4772260a9e
fanny2203 <fanny.cryf.meddwl@gmail.com>
2023-06-01 14:24:46 -0500

    finish 1

--- src/redux/slices/theme.js
+++ src/redux/slices/theme.js
@@ -8,7 +8,7 @@ const themeSlice = createSlice({
     tertiaryColor: "#1C1E45",
     background: "#1f2251",
     error: "#fd5d93",
-    warning: "#ff8d72",
+    warning: "#ffce72",
     success: "#00bf9a",
     btnPrimary: "linear-gradient(to bottom left,#1d8cf8,#3358f4,#1d8cf8)",
     btnDanger: "linear-gradient(to bottom left,#fd5d93,#ec250d,#fd5d93)",
